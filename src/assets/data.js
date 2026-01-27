fd6576857915d7172c3e47b1d861b48b06b5817f
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2026-01-27 20:19:11 +0500

    products datas created

---
+++ src/assets/data.js
@@ -0,0 +1,83 @@
+import Bir from "./1.png";
+import Ikki from "./2.png";
+import Uch from "./3.png";
+import Tort from "./4.png";
+import Besh from "./5.png";
+import Olti from "./6.png";
+import Yetti from "./7.png";
+import Sakkiz from "./8.png";
+
+export const products = [
+  {
+    id: 1,
+    title: "Chair 1",
+    price: 600,
+    stock: 22,
+    rating: 4.2,
+    liked: false,
+    image: Bir,
+  },
+  {
+    id: 2,
+    title: "Chair 2",
+    price: 659,
+    stock: 19,
+    rating: 4.7,
+    liked: false,
+    image: Ikki,
+  },
+  {
+    id: 3,
+    title: "Chair 3",
+    price: 69,
+    stock: 69,
+    rating: 5,
+    liked: false,
+    image: Uch,
+  },
+  {
+    id: 4,
+    title: "Chair 4",
+    price: 312,
+    stock: 32,
+    rating: 3,
+    liked: false,
+    image: Tort,
+  },
+  {
+    id: 5,
+    title: "Chair 5",
+    price: 747,
+    stock: 3,
+    rating: 3,
+    liked: false,
+    image: Besh,
+  },
+  {
+    id: 6,
+    title: "Chair 6",
+    price: 129,
+    stock: 19,
+    rating: 2,
+    liked: false,
+    image: Olti,
+  },
+  {
+    id: 7,
+    title: "Chair 7",
+    price: 57,
+    stock: 20,
+    rating: 2.5,
+    liked: false,
+    image: Yetti,
+  },
+  {
+    id: 8,
+    title: "Chair 8",
+    price: 18,
+    stock: 40,
+    rating: 2,
+    liked: false,
+    image: Sakkiz,
+  },
+];
