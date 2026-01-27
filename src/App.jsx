bd70b52479b803a1848ff34ec540f0da52b7bf60
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2026-01-27 19:55:59 +0500

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Favorites from "./pages/Favorites";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
 }
