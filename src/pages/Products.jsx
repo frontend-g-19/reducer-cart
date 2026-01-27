117d9bcd401c5ae9bd74c9a6a7c208c06279bfa3
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2026-01-27 20:19:29 +0500

    displayed titles and images to the screen

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { products } from "../assets/data";
 
 export default function Products() {
   return (
-    <div>Products</div>
-  )
+    <div>
+      {products.map((mahsulot) => (
+        <div key={mahsulot.id}>
+          <h1>{mahsulot.title}</h1>
+          <img src={mahsulot.image} alt={mahsulot.title} />
+        </div>
+      ))}
+    </div>
+  );
 }
