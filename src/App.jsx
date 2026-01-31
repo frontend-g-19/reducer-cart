ae23f2abd284fc0f070fabda95e869e6abf4b2dd
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2026-01-31 19:44:11 +0500

    provided components

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import Navbar from "./components/Navbar";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import Favorites from "./pages/Favorites";
+import { FavoritesProvider } from "./context/FavoritesContext";
 
 export default function App() {
   return (
-    <>
+    <FavoritesProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Products />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
-    </>
+    </FavoritesProvider>
   );
 }
